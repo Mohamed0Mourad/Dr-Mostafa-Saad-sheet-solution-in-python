a7515eb0705992c0603e914153d309ac2f71d120
Mohamed Mourad <mohamed0mourad@gmail.com>
2024-01-24 21:48:59 +0200

    Create 236A-Div2-boy and girl.py

---
+++ 236A-Div2-boy and girl.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    s = input()
+    st = set()
+    for c in s:
+        st.add(c)
+    if len(st) % 2 == 0:
+        print("CHAT WITH HER!")
+    else:
+        print("IGNORE HIM!")
